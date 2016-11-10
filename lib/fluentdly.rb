461b466ab7ae2fbbe5e2403b593cfc521e53038e
Diego Lendoiro <diego.lendoiro@openhost.es>
2016-11-10 10:55:09 +0000

    Initial commit

---
+++ lib/fluentdly.rb
@@ -0,0 +1,5 @@
+require "fluentdly/version"
+
+module Fluentdly
+  # Your code goes here...
+end
---
+++ spec/fluentdly_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Fluentdly do
+  it 'has a version number' do
+    expect(Fluentdly::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
