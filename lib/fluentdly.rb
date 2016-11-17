5486b3b0bbed1066201aa6a35aa6c0abfad90714
Diego Lendoiro <diego.lendoiro@openhost.es>
2016-11-17 13:01:38 +0000

    added log method

--- lib/fluentdly.rb
+++ lib/fluentdly.rb
@@ -1,5 +1,26 @@
 require "fluentdly/version"
 
 module Fluentdly
-  # Your code goes here...
+  class Logger
+
+    def initialize config
+      @host     = config.fetch(:host,     'localhost')
+      @port     = config.fetch(:port,     24224)
+      @app_name = config.fetch(:app_name, 'myapp')
+    end
+
+    def log severity, content
+      message = content.merge(:severity => severity)
+      adapter.log_post(message)
+    end
+
+    private
+
+    attr_reader :host, :port, :app_name
+
+    def adapter
+      @adapter ||= Fluent::Logger::FluentLogger.new(app_name, :host=>host, :port=>port)
+    end
+
+  end
 end
--- spec/fluentdly_spec.rb
+++ spec/fluentdly_spec.rb
@@ -1,11 +1,26 @@
 require 'spec_helper'
+require 'fluent-logger'
 
-describe Fluentdly do
-  it 'has a version number' do
-    expect(Fluentdly::VERSION).not_to be nil
+describe Fluentdly::Logger do
+
+  let(:config) { {:host => '172.17.12.0', :port => 24224} }
+
+  subject { described_class.new(config) }
+
+  let(:adapter) { double 'adapter' }
+
+  before do
+    allow(Fluent::Logger::FluentLogger).to receive(:new).
+      with('myapp',config).and_return(adapter)
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '#log' do
+    it 'logs properly' do
+      expect(adapter).to receive(:log_post).
+        with(:severity => :info, :foo => 'bar')
+
+      subject.log(:info, :foo => 'bar')
+    end
   end
+
 end
