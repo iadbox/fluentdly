eeed061f1bdaabbb1f9bc5d7be65aecfd0cc55ec
Diego Lendoiro <diego.lendoiro@openhost.es>
2016-11-25 12:01:25 +0000

    configuration class endpoints

--- lib/fluentdly.rb
+++ lib/fluentdly.rb
@@ -1,6 +1,15 @@
 require "fluentdly/version"
 require "fluentdly/logger"
 require "fluentdly/task"
+require "fluentdly/configuration"
 
 module Fluentdly
+
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
+
+  def self.configure
+    yield configuration
+  end
 end
