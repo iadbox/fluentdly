2c01b2a2aafea5dba4eca39475a55fb8c4ada2a4
Diego Lendoiro <diego@iadbox.com>
2017-02-16 11:38:41 +0100

    added message logging for tasks

--- spec/fluentdly/task_spec.rb
+++ spec/fluentdly/task_spec.rb
@@ -7,17 +7,19 @@ describe Fluentdly::Task do
   let(:block) { -> { ['ack', :fake_result] } }
   let(:logger) { double 'logger' }
   let(:config) { double 'config', :task_logger => logger}
+  let(:fake_message) { 'fake_message' }
 
   subject  { described_class.new(severity, parameters, block, config) }
 
   before do
     allow(Time).to receive(:now).and_return 1
+    allow(subject).to receive(:format_message).and_return fake_message
   end
 
   describe '#call' do
     it 'sends data to logger' do
       expect(logger).to receive(:log).
-        with(:info, :foo => 'bar', :status => 'ack', :time => 0)
+        with(:info, :foo => 'bar', :status => 'ack', :time => 0, :message => fake_message)
 
       expect(subject.call).to eq :fake_result
     end
